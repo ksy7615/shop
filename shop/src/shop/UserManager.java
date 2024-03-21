90b4519a79007ece1e0dd884de04b964bc7814bc
ksy7615 <sykim7615@daum.net>
2024-03-21 18:17:54 +0900

    Add getUserById Of class UserManager

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -3,13 +3,13 @@ package shop;
 import java.util.ArrayList;
 
 public class UserManager {
-	
+
 	private ArrayList<User> userList;
-	
+
 	public UserManager() {
 		userList = new ArrayList<>();
 	}
-	
+
 	// 기능 CRUD
 	// C 회원가입
 	public User createUser(String id, String password, String name) {
@@ -17,5 +17,13 @@ public class UserManager {
 		userList.add(user);
 		return user.clone();
 	}
-	
+
+	// id 안겹치게 할 거니까 아이디 조회
+	public User getUserById(String id) {
+		for (User user : userList) {
+			if (user.getId().equals(id))
+				return user;
+		}
+		return new User();
+	}
 }
