3af87d77fa150da6c8455e98d8acae0d590b7d8b
ksy7615 <sykim7615@daum.net>
2024-03-21 18:29:41 +0900

    Add Getter&Setter Of class Item

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -1,5 +1,32 @@
 package shop;
 
 public class Item {
-
+	private String itemName;
+	private int itemCount;
+	
+	public Item() {
+		
+	}
+	
+	public Item(String itemName, int itemCount) {
+		this.itemName = itemName;
+		this.itemCount = itemCount;
+	}
+	
+	public String getItemName() {
+		return this.itemName;
+	}
+	
+	public int getItemCount() {
+		return this.itemCount;
+	}
+	
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+	
+	public Item clone() {
+		return new Item(this.itemName, this.itemCount);
+	}
+	
 }
