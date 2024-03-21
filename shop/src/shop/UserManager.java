bea65b91cde0fd87838988ebea0a6cb0ff8a7974
ksy7615 <sykim7615@daum.net>
2024-03-21 18:17:17 +0900

    Add createUser Of class UserManager

--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -1,6 +1,21 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class UserManager {
-
-
+	
+	private ArrayList<User> userList;
+	
+	public UserManager() {
+		userList = new ArrayList<>();
+	}
+	
+	// 기능 CRUD
+	// C 회원가입
+	public User createUser(String id, String password, String name) {
+		User user = new User(id, password, name);
+		userList.add(user);
+		return user.clone();
+	}
+	
 }
