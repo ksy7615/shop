cb7d22885ad9e4bf29ec55dd9b46418668859745
ksy7615 <sykim7615@daum.net>
2024-03-21 16:22:38 +0900

    Add Class User Getter

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -1,7 +1,30 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class User {
 	private String id, password, name;
 	
+	public User(String id, String password, String name) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public User clone() {
+		return new User(this.id, this.password, this.name);
+	}
+	
 }
