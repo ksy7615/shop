1d752b7946685841a281f5d7d9d16624155dddec
ksy7615 <sykim7615@daum.net>
2024-03-22 11:40:32 +0900

    Modify Constructor +  Getter&Setter Of class Item

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -4,15 +4,17 @@ public class Item {
 	private String itemName;
 	private int price;
 	private int itemCode;
+	private int itemCount;
 	
 	public Item() {
 		
 	}
 	
-	public Item(String itemName, int price, int itemCode) {
+	public Item(String itemName, int price, int itemCode, int itemCount) {
 		this.itemName = itemName;
 		this.price = price;
 		this.itemCode = itemCode;
+		this.itemCount = itemCount;
 	}
 	
 	public String getItemName() {
@@ -27,8 +29,20 @@ public class Item {
 		return this.itemCode;
 	}
 	
+	public int getItemCount() {
+		return this.itemCount;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+	
 	public Item clone() {
-		return new Item(this.itemName, this.price, this.itemCode);
+		return new Item(this.itemName, this.price, this.itemCode, this.itemCount);
 	}
 	
 }
