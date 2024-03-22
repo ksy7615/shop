f1b5ec030ed9425268e3d8289514181499a4e1ea
ksy7615 <sykim7615@daum.net>
2024-03-22 16:06:24 +0900

    Add addCart Of Class Cart

--- shop/src/shop/Cart.java
+++ shop/src/shop/Cart.java
@@ -1,6 +1,25 @@
 package shop;
 
-public class Cart {
+import java.util.ArrayList;
 
+public class Cart {
+	private ArrayList<Item> list;
+
+	public Cart() {
+		list = new ArrayList<>();
+	}
+
+	// CRUD
+
+	// C
+	public Item addCart() {
+		for (Item item : list) {
+			if (item.getItemCode() != 0) {
+				list.add(item);
+				return item.clone();
+			}
+		}
+		return new Item();
+	}
 
 }
