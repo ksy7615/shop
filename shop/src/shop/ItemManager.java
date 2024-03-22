ad38e0cd3128fdffeb935547cb85b62660b2a6fc
ksy7615 <sykim7615@daum.net>
2024-03-22 09:57:00 +0900

    Add Default Constructor Of class ItemManager

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -1,5 +1,13 @@
 package shop;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ItemManager {
+	private ArrayList<Item> itemList;
+
+	public ItemManager() {
+		itemList = new ArrayList<>();
+	}
 
 }
