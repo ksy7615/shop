eb267b612f5f8fa49cfb319a4783924b257ddd4c
ksy7615 <sykim7615@daum.net>
2024-03-25 01:04:59 +0900

    Add loadFile Of Class FileManager (*parsing Modification required)

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -34,4 +34,27 @@ public class FileManager {
 		}
 	}
 	
+	public void loadFile() {
+		if (file.exists()) {
+			String data = "";
+
+			try {
+				fileReader = new FileReader(file);
+				bufferedReader = new BufferedReader(fileReader);
+
+				while (bufferedReader.ready()) {
+					data += bufferedReader.readLine() + "\n";
+				}
+
+				bufferedReader.close();
+				fileReader.close();
+
+				System.out.println("파일로드 성공");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("파일로드 실패");
+			}
+		}
+	}
+	
 }
