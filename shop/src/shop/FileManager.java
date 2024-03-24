6fd49eedb569e57006dbe151aebd23b5313a7a31
ksy7615 <sykim7615@daum.net>
2024-03-25 01:02:01 +0900

    Add 'saveFile' Of Class FileManager (*Data modification required)

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileManager {
 	private FileWriter fileWriter;
@@ -12,5 +13,25 @@ public class FileManager {
 
 	private File file;
 	private String fileName;
+	
+	public FileManager() {
+		this.fileName = "shop.txt";
+		this.file = new File(fileName);
+	}
+	
+	public void saveFile() {
+		String data = "";
 
+		try {
+			fileWriter = new FileWriter(file);
+			fileWriter.write(data);
+			fileWriter.close();
+
+			System.out.println("파일저장 성공");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("파일저장 실패");
+		}
+	}
+	
 }
