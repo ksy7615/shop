8b8f855311c19252d3d386137e4a96ebdeefbd92
ksy7615 <sykim7615@daum.net>
2024-03-25 10:27:30 +0900

    Modify 'Separate itemFile/userFile' Of Class FileManager

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -11,35 +11,49 @@ public class FileManager {
 	private FileReader fileReader;
 	private BufferedReader bufferedReader;
 
-	private File file;
-	private String fileName;
+	private File itemFile;
+	private File userFile;
+	private String itemFileName;
+	private String userFileName;
 	
 	public FileManager() {
-		this.fileName = "shop.txt";
-		this.file = new File(fileName);
+		this.itemFileName = "item.txt";
+		this.itemFile = new File(itemFileName);
+		this.userFileName = "user.txt";
+		this.userFile = new File(userFileName);
 	}
 	
-	public void saveFile() {
-		String data = "";
+	public void saveItemFile() {
+		String data = createItemData();
 
 		try {
-			fileWriter = new FileWriter(file);
+			fileWriter = new FileWriter(itemFile);
 			fileWriter.write(data);
 			fileWriter.close();
 
-			System.out.println("파일저장 성공");
+			System.out.println("아이템 파일저장 성공");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.err.println("파일저장 실패");
+			System.err.println("아이템 파일저장 실패");
 		}
 	}
 	
-	public void loadFile() {
-		if (file.exists()) {
+	private String createItemData() {
+		String data = "";
+		
+		// <아이템> 아이템명/갯수/가격
+		// <유저> 이름/아이디/패스워드
+		//		 구매내역/갯수
+		
+		return data;
+	}
+	
+	public void loadItemFile() {
+		if (itemFile.exists()) {
 			String data = "";
 
 			try {
-				fileReader = new FileReader(file);
+				fileReader = new FileReader(itemFile);
 				bufferedReader = new BufferedReader(fileReader);
 
 				while (bufferedReader.ready()) {
